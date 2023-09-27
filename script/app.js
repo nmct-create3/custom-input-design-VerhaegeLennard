2998228398df73e874fbb50bf7055cb1a0796449
VerhaegeLennard <lennard.verhaege@student.howest.be>
2023-09-27 15:23:52 +0200

    klaar met password, nu darktheme

--- script/app.js
+++ script/app.js
@@ -1,14 +1,20 @@
 const handlePasswordSwitcher = function () {
     console.log("handlePasswordSwitcher executed");
-    const js_password = document.querySelector(".js-password-input")
-    const js_toggle = document.querySelector(".js-password-toggle-checkbox")
-    js_toggle.addEventListener('click', function (){
-        console.log("Klik")
-        if (js_password.type == 'text') {
-            js_password.type = 'password'
-        } else if (js_password.type == 'password') {
-            js_password.type = 'text' }
-    })
+    const passwordOptions = ['password', 'text'];
+    const passwordInput = document.querySelector(".js-password-input")
+    const passwordToggle = document.querySelector(".js-password-toggle-checkbox")
+
+    passwordToggle.addEventListener('change', function () {
+        passwordInput.type = passwordOptions[+this.checked];
+    });
+
+    // js_toggle.addEventListener('click', function (){
+    //     console.log("Klik")
+    //     if (js_password.type == 'text') {
+    //         js_password.type = 'password'
+    //     } else if (js_password.type == 'password') {
+    //         js_password.type = 'text' }
+    // })
 }
 
 const init = function () {
