f0eb54025628a16ff6a4e0497ae7de9bf1841e14
VerhaegeLennard <lennard.verhaege@student.howest.be>
2023-09-27 12:26:37 +0200

    password done

--- script/app.js
+++ script/app.js
@@ -1,5 +1,14 @@
 const handlePasswordSwitcher = function () {
     console.log("handlePasswordSwitcher executed");
+    const js_password = document.querySelector(".js-password-input")
+    const js_toggle = document.querySelector(".js-password-toggle-checkbox")
+    js_toggle.addEventListener('click', function (){
+        console.log("Klik")
+        if (js_password.type == 'text') {
+            js_password.type = 'password'
+        } else if (js_password.type == 'password') {
+            js_password.type = 'text' }
+    })
 }
 
 const init = function () {
